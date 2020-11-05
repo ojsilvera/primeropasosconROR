59f296c1ce82bab5cea7c138b0dff5b87a51f2eb
Oscar Silvera <ojsilvera@misena.edu.co>
2020-11-05 14:22:34 -0500

    se configuro base de datos

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module App
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # ciclo para localizar archivo .env en la carpeta config
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value.to_s
+      end if File.exists?(env_file)
+    end
   end
 end
