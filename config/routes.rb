9c46ec0c540191040b03f7fbd62c3700d526f0ba
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-08 12:53:59 -0500

    crear modelos, task, user, category y migrar DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :institucions
   # get '/hola', to: 'welcome#hello'
-  root 'welcome#hello'
+  # root 'welcome#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
