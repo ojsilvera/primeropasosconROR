ff38b5b1589d8465e0d3e4d96e381b7d589ee71e
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-11 14:22:32 -0500

    ToDo: verificar cuando la tarea posee mas de un follower, completada y funcionando

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,6 @@ class Ability
     can :manage, Task, owner_id: user.id
 
     # * si el usuario es follower del post puede verlo, editarlo y borrarlo
-    # ToDo: verificar cuando la tarea posee mas de un follower
     can :read, Task do |t|
       task = t.participating_user_ids
       user = user.id
