743712869a84da519786056935994be6ba22a226
Oscar Silvera <ojsilvera@misena.edu.co>
2021-02-11 11:31:50 -0500

    Agregar ToDo en abillity sobre multiples followers

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # si el usuario es responsible del post puede verlo, editarlo y borrarlo
+    # * si el usuario es responsible del post puede verlo, editarlo y borrarlo
     can :manage, Task, owner_id: user.id
 
-    # si el usuario es follower del post puede verlo, editarlo y borrarlo
+    # * si el usuario es follower del post puede verlo, editarlo y borrarlo
+    # ToDo: verificar cuando la tarea posee mas de un follower
     can :read, Task do |t|
       task = t.participating_user_ids
       user = user.id
